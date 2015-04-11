8825d2e15c9751a9606ea6830d46b2973582100d
Victoria Gu <vgu@college.harvard.edu>
2015-04-11 10:15:19 -0400

    adding classes

---
+++ classes.py
@@ -0,0 +1,72 @@
+import math
+import matplotlib.pyplot as plt 
+import numpy as np
+
+class Vertex:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.visited = False
+    def EuclidDist(self, vertex):
+        return math.sqrt(pow(self.x - vertex.x, 2) + pow(self.y - vertex.y, 2))
+        
+class Edge:
+    def __init__(self, startv, endv, maxv, iscurve):
+        self.starv = startv
+        self.endv = endv
+        self.maxv = maxv
+        self.iscurve = iscurve
+
+class Graph:
+    def __init__(self):
+        vertexlst = []
+        adjmatrix = [] # will be a nested list of form [[1, 0], [0, 1]], also a symmetric matrix
+        
+    def addVertex(self, vertex):
+        vertexlst.append(vertex)
+        for row in adjmatrix:
+            row.append(0) # add a new column
+        adjmatrix.append([0 * len(vertexlst)]) # add a new row (there's probably a more efficient way)
+        
+    def addEdge(self, edge, vertexind1, vertexind2):
+        adjmatrix[vertexind1][vertexind2] = edge # assuming this is pass by reference
+    
+    # return vertex with averaged x and y coordinates
+    def avgVertex(vertex1, vertex2):
+        meanx = vertex1.x + (vertex2.x - vertex1.x)/2
+        meany = vertex1.y + (vertex2.y - vertex1.y)/2
+        newv = Vertex(meanx, meany)
+        return newv
+        
+def tests():
+    v1 = Vertex(2,3)
+    v2 = Vertex(0,0)
+    dist = v1.EuclidDist(v2)
+    e = Edge(v1, v2, dist, False)
+    graph = Graph()
+    graph.addVertex(v1)
+    print(graph) # TODO: may want to make print function for Graph
+    graph.addVertex(v1)
+    print(graph)
+    graph.addEdge(e)
+    print(graph)
+    
+    
+        
+def main():
+    # convert tiff file to binary array
+    im = plt.imread(tiff_file) # converts to type numpy.ndarray
+    nrow = im.shape[0]
+    ncol = im.shape[1]
+    binim = np.zeros((nrow, ncol)) # 2D binary array
+    for row in range(nrow):
+        for col in range(ncol):
+            # 1 if black, 0 if white
+            binim[row][col] = 1 if 0 in im[row][col] else 0
+    
+    # thin binary array to unit thickness
+    
+    # walk through every row in 2D array until see something black
+
+if __name__ == "__main__":
+    main()
