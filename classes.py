31cd54fad9864e15c700be7754400d7d22d38f88
Victoria Gu <vgu@college.harvard.edu>
2015-04-11 10:48:05 -0400

    reorg main file

--- classes.py
+++ classes.py
@@ -51,22 +51,3 @@ def tests():
     graph.addEdge(e)
     print(graph)
     
-    
-        
-def main():
-    # convert tiff file to binary array
-    im = plt.imread(tiff_file) # converts to type numpy.ndarray
-    nrow = im.shape[0]
-    ncol = im.shape[1]
-    binim = np.zeros((nrow, ncol)) # 2D binary array
-    for row in range(nrow):
-        for col in range(ncol):
-            # 1 if black, 0 if white
-            binim[row][col] = 1 if 0 in im[row][col] else 0
-    
-    # thin binary array to unit thickness
-    
-    # walk through every row in 2D array until see something black
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,22 @@
+from classes import Graph, Edge, Vertex
+
+def main():
+    """ for Tamil characters dataset
+    # convert tiff file to binary array
+    im = plt.imread(tiff_file) # converts to type numpy.ndarray
+    nrow = im.shape[0]
+    ncol = im.shape[1]
+    binim = np.zeros((nrow, ncol)) # 2D binary array
+    for row in range(nrow):
+        for col in range(ncol):
+            # 1 if black, 0 if white
+            binim[row][col] = 1 if 0 in im[row][col] else 0
+    """
+    graph = Graph()
+
+    # thin binary array to unit thickness
+
+    # walk through every row in 2D array until see something black
+
+if __name__ == "__main__":
+    main()
