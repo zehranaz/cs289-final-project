46b09c74ae47b895757774ae86160e55b8520a0e
zehranaz <zehranaz@college.harvard.edu>
2015-04-12 17:40:14 -0400

    updated classes.py functions to catch errors. Probably needs even more testing (i.e. weird corner cases).

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import math
 import matplotlib.pyplot as plt 
 import numpy as np
+import __builtin__
 
 class Vertex:
     def __init__(self, x, y):
@@ -9,45 +10,87 @@ class Vertex:
         self.visited = False
     def EuclidDist(self, vertex):
         return math.sqrt(pow(self.x - vertex.x, 2) + pow(self.y - vertex.y, 2))
-        
+    def get_x(self):
+        return self.x
+    def get_y(self):
+        return self.y
+    def print_out(self):
+        return str(self.x) + ", " + str(self.y)
+
 class Edge:
-    def __init__(self, startv, endv, maxv, iscurve):
-        self.starv = startv
+    def __init__(self, startv, endv, max_dist, iscurve):
+        self.startv = startv
         self.endv = endv
-        self.maxv = maxv
+        self.max_dist = max_dist
         self.iscurve = iscurve
+    def get_startv(self):
+        return self.startv
+    def get_endv(self):
+        return self.endv
+    def get_max_dist(self):
+        return self.max_dist
+    def print_out(self):
+        return "Edge Info - Start: " + self.startv.print_out() + ", End: " + self.endv.print_out() + ", Max Dist: " + str(self.max_dist) + ", is Curve: " + str(self.iscurve)
 
 class Graph:
     def __init__(self):
-        vertexlst = []
-        adjmatrix = [] # will be a nested list of form [[1, 0], [0, 1]], also a symmetric matrix
+        self.vertexlst = []
+        self.adjmatrix = [] # will be a nested list of form [[1, 0], [0, 1]], also a symmetric matrix
         
     def addVertex(self, vertex):
-        vertexlst.append(vertex)
-        for row in adjmatrix:
-            row.append(0) # add a new column
-        adjmatrix.append([0 * len(vertexlst)]) # add a new row (there's probably a more efficient way)
+        num_vertices = len(self.vertexlst)
+        # print "Vert len before " + str(num_vertices)
         
-    def addEdge(self, edge, vertexind1, vertexind2):
-        adjmatrix[vertexind1][vertexind2] = edge # assuming this is pass by reference
-    
+        self.vertexlst.append(vertex)
+        # print "Vert len after " + str(len(self.vertexlst))
+
+        # adding initial vertex
+        if num_vertices == 0:
+            self.adjmatrix = [[None]]
+        else: 
+            for row in self.adjmatrix:
+                row.append(None)
+            new_empty_row = [None for x in range(len(self.vertexlst))]
+            self.adjmatrix.append(new_empty_row)  # add a new row 
+                    
+        print "Dim of matrix after " + str(len(self.adjmatrix)) + " " + str(len(self.adjmatrix[0]))
+        
+    # find the index of each vertex in the vertex list and use it to locate it in the adj matrix   
+    def addEdge(self, edge):
+        start_ind = self.vertexlst.index(edge.get_startv())
+        end_ind = self.vertexlst.index(edge.get_endv())
+        # print start_ind
+        # print end_ind
+        # print "Matrix dim are: "
+        # print len(self.adjmatrix)
+        # print len(self.adjmatrix[0])
+
+        # find index of start and end vertexes in vertex list, add edge both ways
+        start_i = self.vertexlst.index(edge.get_startv())   
+        end_i = self.vertexlst.index(edge.get_endv())
+        self.adjmatrix[start_i][end_i] = edge # assuming this is pass by reference
+        self.adjmatrix[end_i][start_i] = edge
+
     # return vertex with averaged x and y coordinates
     def avgVertex(vertex1, vertex2):
         meanx = vertex1.x + (vertex2.x - vertex1.x)/2
         meany = vertex1.y + (vertex2.y - vertex1.y)/2
         newv = Vertex(meanx, meany)
         return newv
+    def print_adjmatrix(self):
+        print "Adjacency Matrix"
+        for row in self.adjmatrix:
+            for col in row:
+                if col == None:
+                    print col
+                else:
+                    print col.print_out()
+            print 
+    def print_vertexlst(self):
+        print "List of Vertexes"
+        for elt in self.vertexlst:
+            print elt.print_out()
+        print 
+
         
-def tests():
-    v1 = Vertex(2,3)
-    v2 = Vertex(0,0)
-    dist = v1.EuclidDist(v2)
-    e = Edge(v1, v2, dist, False)
-    graph = Graph()
-    graph.addVertex(v1)
-    print(graph) # TODO: may want to make print function for Graph
-    graph.addVertex(v1)
-    print(graph)
-    graph.addEdge(e)
-    print(graph)
     
--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 from classes import Graph, Edge, Vertex
 
+def tests():
+    v1 = Vertex(2,3)
+    v2 = Vertex(0,0)
+    v1.print_out()
+    v2.print_out()
+    print "\n"
+    
+    dist = v1.EuclidDist(v2)
+    e = Edge(v1, v2, dist, False)
+
+    graph = Graph()
+
+    graph.addVertex(v1)
+    # graph.print_adjmatrix()
+    # graph.print_vertexlst()
+
+    graph.addVertex(v2)
+    # graph.print_adjmatrix()
+    # graph.print_vertexlst()
+
+    graph.addEdge(e)
+    graph.print_adjmatrix()
+    graph.print_vertexlst()
+
+    v3 = Vertex(4,5)
+    graph.addVertex(v3)
+    e2 = Edge(v1, v3, v3.EuclidDist(v1), True)
+    graph.addEdge(e2)
+    print("Real Print starts here ")
+    graph.print_adjmatrix()
+    graph.print_vertexlst()
+
+
 def main():
     """ for Tamil characters dataset
     # convert tiff file to binary array
@@ -13,6 +46,7 @@ def main():
             binim[row][col] = 1 if 0 in im[row][col] else 0
     """
     graph = Graph()
+    tests()
 
     # thin binary array to unit thickness
 
